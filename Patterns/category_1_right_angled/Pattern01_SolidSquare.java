9c72abe6248108d36e0e46804a51629f215b1dae
SubhrakantSahu <subhrakantsahu321@gmail.com>
2026-08-02 13:25:05 +0530

    Refactor: Added a print statement after input  to earlier pattern files

--- Patterns/category_1_right_angled/Pattern01_SolidSquare.java
+++ Patterns/category_1_right_angled/Pattern01_SolidSquare.java
@@ -13,6 +13,7 @@ public static void main(String[] Args){
     System.out.println("Print the count ");
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
+     System.out.println("Your pattern is ");
     for(int i = 1; i<=n;i++){
         for ( int j = 1 ;j<=n ; j++){
             System.out.print("* ");
--- Patterns/category_1_right_angled/Pattern03_RightTriangleNumber.java
+++ Patterns/category_1_right_angled/Pattern03_RightTriangleNumber.java
@@ -13,6 +13,7 @@ public class Pattern03_RightTriangleNumber {
     System.out.println("Enter the no count ");
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
+     System.out.println("Your pattern is ");
     for (int i = 1; i<= n ; i++){
         for (int j = 1; j<= i ; j++){
             System.out.print(j+" ");
