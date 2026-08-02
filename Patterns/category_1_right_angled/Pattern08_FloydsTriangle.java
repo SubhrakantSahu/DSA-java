c9f471285e99772eb0c5a91827fdcab50d80e4f8
SubhrakantSahu <subhrakantsahu321@gmail.com>
2026-08-02 17:46:30 +0530

    Refactor: Updated all previous pattern files and comment headers

--- Patterns/category_1_right_angled/Pattern08_FloydsTriangle.java
+++ Patterns/category_1_right_angled/Pattern08_FloydsTriangle.java
@@ -1,32 +1,23 @@
+
 /*
- * Pattern 07: Binary Alternate Triangle
+ * Pattern 08: Floyd’s Triangle
  * 
  * 1
- * 0 1
- * 1 0 1
- * 0 1 0 1
- * 1 0 1 0 1
+ * 2 3
+ * 4 5 6
+ * 7 8 9 10
+ * 11 12 13 14 15
  * 
  */
-
 import java.util.Scanner;
 
-public class Pattern07_BinaryAlternateTriangle {
+public class Pattern08_FloydsTriangle {
     public static void main(String []Args){
     System.out.println("Enter the no count ");
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
     System.out.println("Your pattern is ");
-    for (int i = 1; i<= n ; i++){
-        for (int j = 1; j<=i ; j++){
-            if((i+j)%2 == 0)
-                System.out.print("1 ");
-            else
-                System.out.print("0 ");
-        
-        }
-        System.out.println();
-    }
+    
     // Another way of doing it is starting i from the reverse side - for the outer loop usiong ternary operator 
 // for (int i = 1; i<= n ; i++){
 //         for (int j = 1; j<=i ; j++){
