7a8f497a763fba550c433e3f7cacb09c6db3a44c
SubhrakantSahu <subhrakantsahu321@gmail.com>
2026-08-02 17:56:40 +0530

    Solved Pattern 11: Capital Alpha Ramp Triangle

--- Patterns/category_1_right_angled/Pattern11_CapitalAlphaRampTriangle.java
+++ Patterns/category_1_right_angled/Pattern11_CapitalAlphaRampTriangle.java
@@ -20,7 +20,7 @@ public class Pattern11_CapitalAlphaRampTriangle {
     
     for( int i =1 ;i<=n;i++){
         for(int j = 1 ; j <= i ; j++){
-            System.out.print((char)(i+64));
+            System.out.print((char)(i+64)+" ");
            
         }
         System.out.println();
