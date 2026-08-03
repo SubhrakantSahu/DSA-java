27e1bc5f642494962bc8cef9feacd404101f99f4
SubhrakantSahu <subhrakantsahu321@gmail.com>
2026-08-03 23:11:17 +0530

    Solved Pattern 23: Inverted Small Alpha Ramp

---
+++ Patterns/CATEGORY_2_INVERTED_RIGHT_ANGLED/Pattern23_InvertedSmallAlphaRamp.java
@@ -0,0 +1,36 @@
+/*
+ * Pattern 23: Inverted Small Alpha-Ramp
+ * 
+ * e e e e e
+ * d d d d
+ * c c c
+ * b b
+ * a
+ * 
+ */
+
+import java.util.Scanner;
+
+public class Pattern23_InvertedSmallAlphaRamp {
+    public static void main(String []Args){
+    System.out.println("Enter the no count ");
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+     System.out.println("Your pattern is ");
+    for (int i = 1; i<= n ; i++){
+        for (int j = 1; j<= n-i+1 ; j++){
+            System.out.print((char)((n-i+1)+96) + " ");
+        }
+        System.out.println();
+    }
+
+    //Another way - playwith the oputer loop
+    // for (int i = n; i >= 1 ; i--){
+    //     for (int j = 1; j<= i ; j++){
+    //         System.out.print((char)(i+96)+" ");
+    //     }
+    //     System.out.println();
+    // }
+     sc.close();
+}
+}
