b522671e7731a33737530b4237c031eecc2cd755
SubhrakantSahu <subhrakantsahu321@gmail.com>
2026-08-04 20:44:58 +0530

    Solved Pattern 27: Centered Star Pyramid

--- Patterns/CATEGORY_3_PYRAMIDS_AND_DIAMONDS/Pattern28_InvertedStarPyramid.java
+++ Patterns/CATEGORY_3_PYRAMIDS_AND_DIAMONDS/Pattern28_InvertedStarPyramid.java
@@ -1,28 +1,25 @@
 /*
- * Pattern 27: Centered Star Pyramid
+ * Pattern 28: Inverted Star Pyramid
  * 
- *         *
- *       * * *
- *     * * * * *
- *   * * * * * * *
- * * * * * * * * *
- * 
+   * * * * * * * * *
+     * * * * * * *
+       * * * * *
+         * * *
+           *
  */
-
-
 
 package CATEGORY_3_PYRAMIDS_AND_DIAMONDS;
 
 import java.util.Scanner;
 
-public class Pattern27_CenteredStarPyramid {
+public class Pattern28_InvertedStarPyramid {
     public static void main(String[]Args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number");
     int n = sc.nextInt();
     System.out.println("Your pasattern is ");
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=n-i;j++){
+    for(int i=1;i<n;i++){
+        for(int j=1;j<n-i;j++){
             System.out.print(" "+" ");//did it by coutning space and stars pattern sequentially 
         }
         for(int j=1;j<= (2*i-1);j++){
@@ -35,20 +32,20 @@ public class Pattern27_CenteredStarPyramid {
     /*no of spaces are decreasing so we will start it with a variable 
     1st line 4 spaces thats n-1 then decreases by 1 
     star starts from 1 increases up to n */
-    // int nsp = n-1,nst=1;
+    int nsp = n-1,nst=1;
     
-    //  for(int i=1;i<n;i++){
-    //     for(int j=1;j<nsp;j++){
-    //         System.out.print(" "+" ");//did it by coutning space and stars pattern sequentially 
-    //     }
-    //     for(int j=1;j<=nst;j++){
-    //         System.out.print("*"+" ");
-    //     }
-    //     nst+=2;
-    //     nsp--;
-    //     System.out.println();
+     for(int i=1;i<n;i++){
+        for(int j=1;j<nsp;j++){
+            System.out.print(" "+" ");//did it by coutning space and stars pattern sequentially 
+        }
+        for(int j=1;j<=nst;j++){
+            System.out.print("*"+" ");
+        }
+        nst+=2;
+        nsp--;
+        System.out.println();
 
-    // }
+    }
     sc.close();
     }
 }
