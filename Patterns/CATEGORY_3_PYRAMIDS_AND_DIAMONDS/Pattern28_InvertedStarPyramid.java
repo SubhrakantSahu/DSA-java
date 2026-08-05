61cad1670f43da030a151a8699dc699ce9f37973
SubhrakantSahu <subhrakantsahu321@gmail.com>
2026-08-05 20:35:27 +0530

    Solved Pattern 28: Inverted Star Pyramid

--- Patterns/CATEGORY_3_PYRAMIDS_AND_DIAMONDS/Pattern28_InvertedStarPyramid.java
+++ Patterns/CATEGORY_3_PYRAMIDS_AND_DIAMONDS/Pattern28_InvertedStarPyramid.java
@@ -1,27 +1,29 @@
 /*
- * Pattern 27: Centered Star Pyramid
+ * Pattern 28: Inverted Star Pyramid
+ /*
+ * Pattern 28: Inverted Star Pyramid
  * 
- *         *
- *       * * *
- *     * * * * *
- *   * * * * * * *
- *  * * * * * * * *
+ * * * * * * * * * * 
+ *   * * * * * * * 
+ *     * * * * * 
+ *       * * * 
+ *         * 
  * 
  */
-
+ 
 
 
 package CATEGORY_3_PYRAMIDS_AND_DIAMONDS;
 
 import java.util.Scanner;
 
-public class Pattern27_CenteredStarPyramid {
+public class Pattern28_InvertedStarPyramid {
     public static void main(String[]Args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number");
     int n = sc.nextInt();
     System.out.println("Your pasattern is ");
-    // for(int i=1;i<=n;i++){
+    // for(int i=n;i>=1;i--){
     //     for(int j=1;j<=n-i;j++){
     //         System.out.print(" "+" ");//did it by coutning space and stars pattern sequentially 
     //     }
@@ -32,10 +34,10 @@ public class Pattern27_CenteredStarPyramid {
 
     // }
     //another method is there 
-    /*no of spaces are decreasing so we will start it with a variable 
-    1st line 4 spaces thats n-1 then decreases by 1 
-    star starts from 1 increases up to n */
-    int nsp=n-1,nst=1;
+/*
+no of space at first line is 0 but increasing so take a constant 
+no of starts decreasing so take a variable */ 
+    int nsp = 0,nst=(2*n)-1;
     
      for(int i=1;i<=n;i++){
         for(int j=1;j<=nsp;j++){
@@ -44,8 +46,9 @@ public class Pattern27_CenteredStarPyramid {
         for(int j=1;j<=nst;j++){
             System.out.print("*"+" ");
         }
-        nst+=2;
-        nsp--;
+        nst-=2;
+        nsp++;
+        
         System.out.println();
 
     }
