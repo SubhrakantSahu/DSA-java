b07c8639c6a4255180ce5dc5af845f0ce8e17af1
SubhrakantSahu <subhrakantsahu321@gmail.com>
2026-08-05 22:16:45 +0530

    Solved Pattern 29: Star Diamond

---
+++ Patterns/CATEGORY_3_PYRAMIDS_AND_DIAMONDS/Pattern29_StarDiamond.java
@@ -0,0 +1,79 @@
+/*
+        *
+      * * *
+    * * * * *
+  * * * * * * *
+* * * * * * * * *
+* * * * * * * * *
+  * * * * * * *
+    * * * * *
+      * * *
+        *
+*/
+
+package CATEGORY_3_PYRAMIDS_AND_DIAMONDS;
+
+import java.util.Scanner;
+
+public class Pattern29_StarDiamond {
+    public static void main(String[]Args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+    int n = sc.nextInt();
+    System.out.println("Your pasattern is ");
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+            System.out.print(" "+" ");
+        }
+        for(int j=1;j<=(2*i)-1;j++){
+            System.out.print("*"+" ");
+        }
+        System.out.println();
+    }
+    //lower pyramid 
+    for(int i=n;i>=1;i--){
+        for(int j=1;j<=n-i;j++){
+            System.out.print(" "+" ");
+        }
+        for(int j=1;j<=(2*i)-1;j++){
+            System.out.print("*"+" ");
+        }
+        System.out.println();
+    }
+
+
+    ///-----------------------------------------------
+    /// we can also use nsp and nst method 
+    //  int nsp=n-1,nst=1;
+    
+    //  for(int i=1;i<=n;i++){
+    //     for(int j=1;j<=nsp;j++){
+    //         System.out.print(" "+" ");//did it by coutning space and stars pattern sequentially 
+    //     }
+    //     for(int j=1;j<=nst;j++){
+    //         System.out.print("*"+" ");
+    //     }
+    //     nst+=2;
+    //     nsp--;
+    //     System.out.println();
+
+    // }
+    // nsp = 0;
+    // nst=(2*n)-1;//if 5 lines were not there after one pyramid then the nst would have been 
+    // //  (2*n)-3//2 star less then its above line in that pattern 1 to 5 line normal stars then 3 stars (a Different pattern)
+    //  for(int i=1;i<=n;i++){
+    //     for(int j=1;j<=nsp;j++){
+    //         System.out.print(" "+" ");//did it by coutning space and stars pattern sequentially 
+    //     }
+    //     for(int j=1;j<=nst;j++){
+    //         System.out.print("*"+" ");
+    //     }
+    //     nst-=2;
+    //     nsp++;
+        
+    //     System.out.println();
+
+    // }
+    sc.close();
+        }
+}
