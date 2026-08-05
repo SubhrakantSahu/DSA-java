96e4bfe697e405bc6193f70ca839be653f817071
SubhrakantSahu <subhrakantsahu321@gmail.com>
2026-08-05 22:24:41 +0530

    Solved Pattern 31: Sequential Number Pyramid

---
+++ Patterns/CATEGORY_3_PYRAMIDS_AND_DIAMONDS/Pattern31_SequentialNumberPyramid.java
@@ -0,0 +1,48 @@
+/*
+        1
+      1 2 3
+    1 2 3 4 5
+  1 2 3 4 5 6 7
+1 2 3 4 5 6 7 8 9
+*/
+package CATEGORY_3_PYRAMIDS_AND_DIAMONDS;
+
+import java.util.Scanner;
+
+public class Pattern31_SequentialNumberPyramid {
+    public static void main(String[]Args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+    int n = sc.nextInt();
+    System.out.println("Your pasattern is ");
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+            System.out.print(" "+" ");//did it by coutning space and stars pattern sequentially 
+        }
+        for(int j=1;j<= (2*i-1);j++){
+            System.out.print(j+" ");
+        }
+        System.out.println();
+
+    }
+    //another method is there 
+    /*no of spaces are decreasing so we will start it with a variable 
+    1st line 4 spaces thats n-1 then decreases by 1 
+    star starts from 1 increases up to n */
+    int nsp=n-1,nst=1;
+    
+     for(int i=1;i<=n;i++){
+        for(int j=1;j<=nsp;j++){
+            System.out.print(" "+" ");//did it by coutning space and stars pattern sequentially 
+        }
+        for(int j=1;j<=nst;j++){
+            System.out.print(j+" ");
+        }
+        nst+=2;
+        nsp--;
+        System.out.println();
+
+    }
+    sc.close();
+    }
+}
